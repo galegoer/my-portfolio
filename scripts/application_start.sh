26edf2700b33f9ac08a3390fc43e3b1e8be8aeab
galegoer <eric.m.galego@gmail.com>
2022-09-29 17:10:50 -0400

    fixing scripts to start app

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -12,7 +12,10 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
+cd my-website/client/
+
 npm install
+npm start > app.out.log 2> app.err.log < /dev/null &
 
 #start our node app in the background
-node app.js > app.out.log 2> app.err.log < /dev/null &
+# node app.js > app.out.log 2> app.err.log < /dev/null &
