68318a25c4559cf47a5790eacd1fab8dd9875f00
galegoer <eric.m.galego@gmail.com>
2022-10-29 12:55:43 -0400

    homepage + navbar

--- my-website/client/src/App.js
+++ my-website/client/src/App.js
@@ -1,63 +1,23 @@
-import './App.css';
-// import RandomVideo from './components/RandomVideo';
-// import NavBar from './components/NavBar';
-// import HomePage from './components/HomePage';
-import Chat from './components/Chat';
-import { useState } from 'react';
+import HomePage from './components/HomePage';
+import JoinRoom from './components/JoinRoom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ErrorPage from './components/ErrorPage';
+import NavBar from './components/NavBar';
 
-import io from 'socket.io-client';
-const socket = io.connect("http://localhost:3000");
 
 function App() {
 
-  const [room, setRoom] = useState("");
-  const [showChat, setChat] = useState(false);
-  const [userName, setUsername] = useState("");
-
-  const joinRoom = () => {
-    if(room !== "") {
-      socket.emit('join_room', room);
-      setChat(true);
-    }
-  };
-
   return (
-    <div className="App">
-      <header className="App-header">
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
 
-        <h3>Active Rooms:</h3>
-        {/* {io.sockets.adapter.rooms.map ( e => 
-          <div style={{color:'green'}}>{e}</div>
-        )} */}
-
-        {/* <NavBar /> */}
-        {/* <HomePage /> */}
-        {!showChat ? (
-          <div className="joinChatContainer">
-            <h3>Join A Chat</h3>
-            <input
-              type="text"
-              placeholder="Enter Username..."
-              onChange={(event) => {
-                setUsername(event.target.value);
-              }}
-            />
-            <input
-              type="text"
-              placeholder="Enter a Room ID..."
-              onChange={(event) => {
-                setRoom(event.target.value);
-              }}
-            />
-            <button onClick={joinRoom}>Join A Room</button>
-          </div>
-        ) : (
-          <Chat socket={socket} username={userName} room={room} />
-        )}
-        {/* Random Video for Today */}
-        {/* <RandomVideo /> */}
-      </header>
-    </div>
+        <Route path="/chat" element={<JoinRoom />} />
+        
+        <Route path="*" element={<ErrorPage />} />
+      </Routes> 
+    </Router>
   );
 }
 
