076e35c78df712f343a80fa0cbe4ac556f4aa889
galegoer <eric.m.galego@gmail.com>
2022-10-31 00:53:55 -0400

    fixing npm path

--- scripts/ecosystem.config.js
+++ scripts/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "server+client",
       cwd: "../my-website/client",
-      script: "/home/ec2-user/.nvm/versions/node/v19.0.0/bin/npm-cli.js",
+      script: "/home/ec2-user/.nvm/versions/node/v19.0.0/lib/node_modules/npm/bin/npm-cli.js",
       args: "start",
     },
   ],
