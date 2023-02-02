b80e5481d9b79ca60448ff63332b35003a959f7c
galegoer <eric.m.galego@gmail.com>
2023-02-02 16:45:48 -0500

    added song recommendation page

--- my-website/client/src/App.js
+++ my-website/client/src/App.js
@@ -3,6 +3,7 @@ import JoinRoom from './components/JoinRoom';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ErrorPage from './components/ErrorPage';
 import NavBar from './components/NavBar';
+import RandomVideo from './components/RandomVideo';
 
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
 
         <Route path="/chat" element={<JoinRoom />} />
+
+        <Route path="/song-rec" element={<RandomVideo />} />
         
         <Route path="*" element={<ErrorPage />} />
       </Routes> 
--- my-website/client/src/components/NavBar.js
+++ my-website/client/src/components/NavBar.js
@@ -7,12 +7,14 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 function NavBar() {
     
     return (
+      <div style={{'height': '5rem'}}>
       <Navbar fixed='top' collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="/">Eric Galego</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
+              <Nav.Link href="/song-rec">Song Recommendation</Nav.Link>
               <Nav.Link href="#features">Game 1 (TBD)</Nav.Link>
               <Nav.Link href="#pricing"></Nav.Link>
               <NavDropdown title="Temp Dropdown" id="collasible-nav-dropdown">
@@ -34,6 +36,7 @@ function NavBar() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+      </div>
     );
 }
 
