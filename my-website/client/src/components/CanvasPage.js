3f0207aace5ae3ee202ab52cde8357063245c2c8
galegoer <eric.m.galego@gmail.com>
2023-02-04 16:34:23 -0500

    added color picker and centered

--- my-website/client/src/components/CanvasPage.js
+++ my-website/client/src/components/CanvasPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import CanvasDraw from "react-canvas-draw";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function CanvasPage() {
   
   const saveableCanvas = useRef(null);
-  // const [color, setColor] = useState('white');
+  const [color, setColor] = useState('white');
 
   const setCanvasRef = (canvasDraw) => {
     saveableCanvas.current = canvasDraw;
@@ -13,42 +13,43 @@ function CanvasPage() {
 
   return (
     <>
-      <CanvasDraw 
+      <CanvasDraw className='mx-auto border border-dark'
         ref={setCanvasRef} 
         onChange={() => console.log("onChange")}
-        // brushColor={color}
-         />
-
+        brushColor={color}
+        brushRadius="15"
+      />
       {/* <button onClick={() => {
         localStorage.setItem(
           "savedDrawing",
           saveableCanvas.current.getSaveData()
         );
       }}>Save</button> */}
+      <div className='d-flex justify-content-center'>
+        <button onClick={() => {
+          saveableCanvas.current.eraseAll();
+        }}>Clear Canvas</button>
 
-      <button onClick={() => {
-        saveableCanvas.current.eraseAll();
-      }}>Clear Canvas</button>
+        <button onClick={() => {
+          saveableCanvas.current.undo();
+        }}>Undo</button>
 
-      <button onClick={() => {
-        saveableCanvas.current.undo();
-      }}>Undo</button>
+        <button onClick={() => {
+          console.log(saveableCanvas.current.getDataURL());
+          alert("DataURL written to console")
+        }}>GetDataURL</button>
 
-      <button onClick={() => {
-        console.log(saveableCanvas.current.getDataURL());
-        alert("DataURL written to console")
-      }}>GetDataURL</button>
-
-      {/* <button onClick={() => {
-        setColor((curr) => {
-          console.log(curr);
-          if(curr === 'white') {
-            return 'black';
-          }else{
-            return 'white';
-          }
-        });
-      }}>Change Color</button> */}
+        <button onClick={() => {
+          setColor((curr) => {
+            console.log(curr);
+            if(curr === 'white') {
+              return 'black';
+            }else{
+              return 'white';
+            }
+          });
+        }}>Change Color</button>
+      </div>
 
     </>
   );
