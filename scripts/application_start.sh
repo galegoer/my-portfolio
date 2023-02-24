eeca9b5e8c1144465626f17fd69889d475dbbd4e
galegoer <eric.m.galego@gmail.com>
2023-02-24 15:21:43 -0500

    navigating to script directory

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -11,7 +11,7 @@ sudo chmod -R 777 /home/ec2-user/node-app
 # sudo npm install pm2 -g
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user/node-app
+cd /home/ec2-user/node-app/scripts
 
 #add npm and node to path
 # export NVM_DIR="$HOME/.nvm"	
