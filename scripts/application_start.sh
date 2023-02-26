6d21dce69de4aba32efffa2d711e24f8f0d5a11a
galegoer <eric.m.galego@gmail.com>
2023-02-26 00:46:55 -0500

    fixing deploy scripts

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -40,19 +40,3 @@ docker-start.sh > /dev/null 2> /dev/null < /dev/null &
 
 # client
 # docker run -p 3000:80 my-website-docker:latest & docker run -p 8000:8000 my-website-docker-server:latest
-
-
-
-# Added
-
-# echo "Pre-Build Steps:"
-# echo "authenticating with AWS ECR..."
-# aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 090413310302.dkr.ecr.us-east-2.amazonaws.com
-
-# echo "Build Steps:"
-# echo "building image..."
-# docker build -t 090413310302.dkr.ecr.us-east-2.amazonaws.com/my-website-docker:latest .
-
-# echo "Post-Build Steps:"
-# echo "pushing image to AWS ECR..."
-# docker push 090413310302.dkr.ecr.us-east-2.amazonaws.com/my-website-docker:latest
