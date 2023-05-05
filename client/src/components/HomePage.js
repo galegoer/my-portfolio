34cc3a8ec3cffabf3534c7fbe6b7961b1cba1829
galegoer <eric.m.galego@gmail.com>
2023-05-04 20:05:23 -0400

    updated homepage with matrix effect

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,23 +1,44 @@
-import TerminalIcon from '@mui/icons-material/Terminal';
 import { useEffect, useRef } from 'react';
 import '../styles/HomePage.css';
 import HomeCard from './HomeCard';
+import MatrixBackground from './Matrix';
 
 
 function HomePage(props) {
 
     const hiddenElsRef = useRef(null);
     const cardsRef = useRef(null);
+    const hackerRef = useRef(null);
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     useEffect(() => {
       const hiddenEls = hiddenElsRef.current.querySelectorAll(".hidden");
       hiddenEls.forEach((el) => observer.observe(el));
+      hackerRef.current.addEventListener('mouseover', onMouseOver);
 
       cardsRef.current.addEventListener('mousemove', onMouseMove);
       return () => {
         cardsRef.current.removeEventListener('mousemove', onMouseMove);
       };
     }, []);
+
+    const onMouseOver = (e) => {
+      let iterations = 0;
+      const interval = setInterval(() => {
+        e.target.innerText = e.target.innerText.split("")
+        .map((letter, index) => {
+          if(index < iterations) {
+            return e.target.dataset.value[index];
+          }
+          return letters[Math.floor(Math.random() * 26)]
+        })
+        .join("");
+
+      if(iterations >= e.target.dataset.value.length) clearInterval(interval);
+      
+      iterations += 1 / 3;
+      }, 30);
+    }
 
     const onMouseMove = (e) => {
       const cards = cardsRef.current.querySelectorAll('.home-card');
@@ -43,26 +64,28 @@ function HomePage(props) {
     
     return (
       <div ref={hiddenElsRef} className="mx-10">
+          <MatrixBackground />
+
         <section className='hidden'>
-          <div className='d-flex flex-column align-items-center text-center'>
+          <div ref={hackerRef} data-value="ERIC GALEGO" className='hacker-txt'>ERIC GALEGO</div>
+
+        </section>
+        <section className='hidden'>
+          <div className='txt-box d-flex flex-column align-items-center text-center'>
             <img height="200" width="200" src="memoji-thumbs-up.png" alt="memoji"/>
             <div className='d-block' style={{fontSize: "30px"}}>
               <span>Hello, Welcome to my Page! 😀 
               I'm Eric a recent University of Toronto Computer Science Specialist graduate!
               Note this page is still under construction 🚧 Many new features are TBD... </span>
             </div>
-            <div className='d-flex justify-content-around w-100'>
-              <img height="100" src="uoft-logo.png" alt="uoft-logo"/>
-              <TerminalIcon style={{ fontSize: 100 }} />
-            </div>
           </div>
         </section>
         <section className='hidden'>
-          <span style={{ fontSize: "40px" }}>Here I'll show some of my projects, some interests of mine, as well as some resources I've found useful throughout the years. </span>
+          <div className='txt-box' style={{ fontSize: "40px" }}>Here I'll show some of my projects, some interests of mine, as well as some resources I've found useful throughout the years. </div>
         </section>
         <section className='hidden'>
           {/* <div className="skills-area"> */}
-          <span className="d-flex justify-content-center" style={{ fontSize: "40px" }}>Some of my Skills ✨</span>
+          <div className="d-flex justify-content-center txt-box mb-4">Some of my Skills ✨</div>
             <div ref={cardsRef} id='cards'>
               <HomeCard src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" alt="python" title="Python" />
               <HomeCard src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg" alt="java" title="Java" />
@@ -83,16 +106,16 @@ function HomePage(props) {
         </section>
         <section className='hidden'>
           <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%" }} >
-            <span style={{ fontSize: "40px" }}>
+            <div className="txt-box" style={{ fontSize: "40px" }}>
               <span>Interests TBD 👋🏻</span>
               <span>Resources TBD 🙋🏻‍♀️</span>
               <span>Projects TBD ✋🏻</span>
               <span>See you</span>
-            </span>
+            </div>
           </div>
         </section>
         <section className='hidden'>
-          <span style={{ fontSize: "40px" }}>That's It... For Now</span>
+          <div className="txt-box" style={{ fontSize: "40px" }}>That's It... For Now</div>
         </section>
       </div>
     );
