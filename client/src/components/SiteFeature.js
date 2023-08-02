c5eeb4e5e97c5d29d9dffe3a8c64c4df7a45297a
galegoer <eric.m.galego@gmail.com>
2023-08-01 23:14:39 -0400

    bg transition and font size on mobile fix

--- client/src/components/SiteFeature.js
+++ client/src/components/SiteFeature.js
@@ -8,13 +8,11 @@ function SiteFeature(props) {
     
     return (
         <div className='project-hover home-card skills-child site-feature text-center'>
-            <div className='project-display'>
-                <div className='flex-column p-4'>
-                    <div className='project-name'>
-                        <a href={siteFeatures[props.name]["projectUrl"]}>{siteFeatures[props.name]["name"]}</a>
-                        <RiArrowRightUpLine className='project-arrow' />
-                    </div>
-                </div>
+            <div className='project-display flex-column p-4'>
+                <h3 className='project-name'>
+                    <a href={siteFeatures[props.name]["projectUrl"]}>{siteFeatures[props.name]["name"]}</a>
+                    <RiArrowRightUpLine className='project-arrow' />
+                </h3>
             </div>
             <div className="p-4">
                 {siteFeatures[props.name]["info"]}
